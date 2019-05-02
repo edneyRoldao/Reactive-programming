fa2a4e7dcf3609ee1b7b4729c9df4088e6f8967a
Edy <edneyroldao@gmail.com>
2019-05-01 23:52:21 -0300

    first example has been created

--- 01-intro/src/code.ts
+++ 01-intro/src/code.ts
@@ -1,3 +1,29 @@
-import * as Rx from 'rxjs';
+import { Observable } from "rxjs/index";
 
-console.log(Rx);
+let observable = Observable.create((observer: any) => {
+    setInterval(() => {
+        observer.next('sending event');
+    }, 2000);
+});
+
+let observer1 = observable.subscribe((x: any) => addItem('I am the first: ' + x));
+
+let observer2 = observable.subscribe((x: any) => {
+    console.log('I am the second:', x);
+})
+
+
+setTimeout(() => {
+    observer1.unsubscribe();
+}, 10000);
+
+setTimeout(() => {
+    observer2.unsubscribe();
+}, 20000);
+
+function addItem(value: any): void {
+    let node  = document.createElement('li');
+    let textNode = document.createTextNode(value);
+    node.appendChild(textNode);
+    document.getElementById('output').appendChild(node);
+}
