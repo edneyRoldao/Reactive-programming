6bb2c3547182a3d969eb7b8d2fac07e55683c835
Edy <edneyroldao@gmail.com>
2019-05-04 18:21:28 -0300

    add

--- 01-intro/src/code.ts
+++ 01-intro/src/code.ts
@@ -1,22 +1,17 @@
-import { fromEvent } from "rxjs";
+import { of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
-// Let's create an observable that will listen to mouse click event
-const observable = fromEvent(document.getElementById('myButton'), 'click');
+const datasource = of(1, 2, 5, 7, 8);
 
+let result: string = '';
 
-/*
-    A subscription (listener) is the guy that will be notified when something happens from observable.
-    Each subscription can react at your own way by given a callback function or object to subscribe method.
-    This function is known as observer.
-*/
-
-const observer1 = (event: any) => console.log(event);
-const subscription1 = observable.subscribe(observer1);
-
-
-let count = 0;
-const observer2 = (event: any) => {
-    document.getElementById('myContent').innerHTML = '' + (++count);
-};
-
-const subscription2 = observable.subscribe(observer2);
+const subscription = datasource
+    .pipe(
+        filter(v => v >= 5),
+        map(v1 => v1 * 2)
+    )
+    .subscribe(finalValue => {
+        console.log(finalValue);
+        result =  result + finalValue + ' - ';
+        document.getElementById('myContent').innerHTML = result;
+    });
